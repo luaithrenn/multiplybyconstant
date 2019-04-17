5f490f80f0e7bc8a9821e779947e642022775f30
luaithrenn <luaidemp@ie.ibm.com>
2019-04-17 21:50:21 +0100

    updated

--- multiplyconstant/multiplybyconstant.py
+++ multiplyconstant/multiplybyconstant.py
@@ -9,15 +9,17 @@ class MultiplyBySix(BaseTransformer):
     '''
     Multiply input column by 6 to produce a result
     '''
+    
+    url = Package_URL
 
-    def __init__(self, input_item = input_item, result ='result'):
+    def _init__(self, input_item, output_item = 'output_item'):
 
         self.input_item = input_item
         self.result = result
-        super().__init__()
+        s
+        uper().__init__()
 
     def execute(self, df):
         df = df.copy()
-        df[self.result] = df[self.input_item] * 6
-
+        df[self.output_item] = df[self.input_item] * 6
         return df
