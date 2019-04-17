f82a2180103f1fafc5fcb113e923a883d33249b9
luaithrenn <luaidemp@ie.ibm.com>
2019-04-17 20:12:42 +0100

    add custom function

---
+++ multiplyconstant/multiplybyconstant.py
@@ -0,0 +1,23 @@
+Package_URL = 'git+https://github.ibm.com/Luaithrenn/multiplybyconstant.git'
+
+import numpy as np
+import pandas as pandas
+from iotfunctions.preprocessor import BaseTransformer
+
+class MultiplyBySix(BaseTransformer):
+
+    '''
+    Multiply input column by 6 to produce a result
+    '''
+
+    def __init__(self, input_item=input_item, result='result'):
+
+        self.input_item = input_item
+        self.result = result
+        super().__init__()
+
+    def execute (self, df)
+        df = df.copy()
+        df[self.result] = df[self.input_item] * 6
+
+        return df
